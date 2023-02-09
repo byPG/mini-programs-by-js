d787929530a12d50feb3cbfb0360a3395cdd3961
byPG <67753466+byPG@users.noreply.github.com>
2023-02-09 12:02:51 +0100

    finish mini project V1

---
+++ clock/main.js
@@ -0,0 +1,27 @@
+class Clock{
+    constructor(){
+        this.hour = document.querySelector(".hour");
+        this.minute= document.querySelector(".minute");
+        this.second = document.querySelector(".second");
+    }
+
+    tick = () => {
+        let date = new Date();
+        let h = date.getHours() * 30;
+        let m = date.getMinutes() * 6;
+        let s = date.getSeconds() * 6;
+
+        this.hour.style.transform = `rotateZ(${h + m/12}deg)`; //obracanie wskazówkis
+        this.minute.style.transform = `rotateZ(${m}deg)`;
+        this.second.style.transform = `rotateZ(${s}deg)`;
+
+        console.log(h, m, s)
+    }
+
+    start(){
+        setInterval(this.tick, 1000);
+    }
+}
+
+const clock = new Clock();
+clock.start();
