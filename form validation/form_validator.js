e78776a8c91423093bc6f1fb3067bf62bb9f0696
byPG <67753466+byPG@users.noreply.github.com>
2023-02-24 22:43:59 +0100

    some changes in JS code

---
+++ form validation/form_validator.js
@@ -0,0 +1,28 @@
+class FormValidator {
+    constructor(){
+        this.formFields = [];
+
+        this.form = document.getElementById("form");
+
+        this.init();
+    }
+
+    init() {
+        this.form.addEventListener("submit", (e) => {
+            e.preventDefault();
+
+            TouchList.validateForm();
+
+        });
+    }
+
+    validateForm = () => {
+        const formResults = this.formFields.map(f => f.validate());
+
+        if(formResults.includes(false)){
+            console.log("Błąd w formularzu");
+        } else{
+            console.log("Formularz jest ok")
+        }
+    }
+}
