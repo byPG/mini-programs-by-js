160a7934a0a52ac5b9095d31b9b2c760248fd380
byPG <67753466+byPG@users.noreply.github.com>
2023-03-05 18:01:51 +0100

    new changes

---
+++ form validation/form_field.js
@@ -0,0 +1,11 @@
+class FormField{
+    constructor(formFieldSelector, {minlength = 3, maxlength = 100, errorrMsgSelector, matchWithPasswordId}){
+
+    this.formField = document.querySelector("formFieldSelector");
+    this.type = this.formField.type;    
+    this.minlength = minlength;
+    this.maxlength = maxlength;
+    this.errorrMsgEl = document.querySelector(errorrMsgSelector);
+    this.matchWithPasswordId = matchWithPasswordId;
+    }
+}
