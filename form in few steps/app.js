8407765df36c5f65ab0e0cfe9b0be85afc413956
byPG <67753466+byPG@users.noreply.github.com>
2023-03-24 08:50:30 +0100

    basic JS for project

---
+++ form in few steps/app.js
@@ -0,0 +1,45 @@
+class MultiStepForm {
+    constructor(){
+    }
+
+    init() {
+        this.form = document.querySelector("form");
+        this.steps = Array.from(document.querySelectorAll("form.step"));
+        this.nextBtn = document.querySelectorAll("form .next-btn");
+        this.prevBtn = document.querySelectorAll("form .previous-btn");
+
+        this.addListeners();
+    }
+
+    addListeners() {
+        this.nextBtn.forEach(btn => {
+            btn.addEventListener("click", () => this.changeStep(1)); //przekazana metoda changeStep (z przekazaniem argumentu)
+        });
+
+        this.prevBtn.forEach(btn => {
+            btn.addEventListener("click", () => { this.changeStep(-1)});
+        });
+
+        this.form.addEventListener("submit", this.submit);
+    }
+    
+    changeStep = (v) => {
+
+
+    }
+
+    resetSteps = () => {
+        this.changeStep(0);
+    }
+
+    submit = (e) => {
+        e.preventDefault();
+        this.form.reset();
+        this.resetSteps();
+    }
+
+    
+}
+
+const multiForm = new MultiStepForm();
+multiForm.init();
